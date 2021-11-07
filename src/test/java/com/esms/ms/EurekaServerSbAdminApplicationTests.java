c9ba5c3e2dc90344ff2d828738a484b72af39c25
CloudAppsDeveloper <developer.cloudapps@gmail.com>
2021-11-07 12:43:28 +0530

    initial  commit

---
+++ src/test/java/com/esms/ms/EurekaServerSbAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.esms.ms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerSbAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
